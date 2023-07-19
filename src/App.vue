e6de51f956d859795355440ed936f48fb0dbf318
AndreiMakrushin <110162575+AndreiMakrushin@users.noreply.github.com>
2023-07-19 15:21:38 +0500

    ee

--- src/App.vue
+++ src/App.vue
@@ -20,63 +20,6 @@ const redact = () => {
     arr.value[renameIndex.value].value = rename.value
   }
 }
-function convertArrayToObject(array) {
-  const result = {};
-  for (const item of array) {
-    result[item.id] = item.count;
-  }
-  return result;
-}
-
-const resources = [
-  {
-    id: 1,
-    count: 13,
-  },
-  {
-    id: 2,
-    count: 5,
-  },
-  {
-    id: 3,
-    count: 24,
-  },
-  {
-    id: 4,
-    count: 101,
-  },
-  {
-    id: 5,
-    count: 72,
-  },
-  {
-    id: 6,
-    count: 64,
-  },
-  {
-    id: 7,
-    count: 305,
-  },
-  {
-    id: 8,
-    count: 67,
-  },
-  {
-    id: 9,
-    count: 95,
-  },
-  {
-    id: 10,
-    count: 21,
-  },
-  {
-    id: 11,
-    count: 37,
-  },
-];
-
-const result = convertArrayToObject(resources);
-console.log(result);
 
 </script>
 
